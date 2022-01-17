7e991ef19dce775e1feed67674ca075faf2b2bb1
YS <yooooonnf@gmail.com>
2022-01-17 23:20:20 +0900

    Initial commit

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,22 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;//Long을 사용하는 이유 NULL이 들어갈수있다 long은 안된다 . 이후에 db사용 가능하기때문
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
